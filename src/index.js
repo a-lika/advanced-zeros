88d29a5b5626afcb7368a2c195c9b2590f06a6c6
Anzhelika <anzhelika110k@gmail.com>
2018-10-08 19:48:54 +0300

    refactor: replace var to let

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation 
-  var current_number = number, base_count = 0, num_count = 0, num; 
-    for(var i = 2; i <= base; i++){
-      if(base % i == 0){
-        base_count = 0;
-		do{ 
-			base_count++;
-            base = Math.floor(base/i);
-		}
-        while(base % i == 0)		
-      num = number;
-	  num_count = 0;
-		do{
-			num_count+=Math.floor(num/i);		  
-			num=Math.floor(num/i);	
-		}
-        while (num/i > 0)
+    let currentNumber = number;
+    let baseCount = 0;
+    let numCount = 0;
+    let num;
 
-     if(current_number > num_count/base_count){ 
-        current_number = num_count/base_count;
-      } 
+    for (let i = 2; i <= base; i++) {
+        if (base % i == 0) {
+            baseCount = 0;
+            do {
+                baseCount++;
+                base = Math.floor(base / i);
+            } while (base % i == 0);
+            num = number;
+            numCount = 0;
+            do {
+                numCount += Math.floor(num / i);
+                num = Math.floor(num / i);
+            } while (num / i > 0);
+
+            if (currentNumber > numCount / baseCount) {
+                currentNumber = numCount / baseCount;
+            }
+        }
     }
-  }
-  return Math.floor(current_number);  
-}
+    return Math.floor(currentNumber);
+};
